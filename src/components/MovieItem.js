0eb53af605777f1a4dc119929a235677ef102013
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-19 22:55:48 -0300

    Estilizar itens de lista

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -8,8 +8,18 @@ export const MovieItem = ({ imdbID, Title, Type, Year, Poster }) => {
 
     return (
         <div className="single-movie">
-            <h4>{Title}</h4>
-            <p>{Year}</p>
+            <div >
+                <div className="movie-icon" >
+                    O
+                </div>
+            </div>
+            <div>
+                <h4 className="movie-title">{Title}</h4>
+                <p className="movie-year">{Year}</p>
+            </div>
+           <div>
+               <span>*</span>
+           </div>
         </div>
     );
 };
